c651f061140e4d985e3307a1eee73415db6b9c63
KINGS=IKE <ogbonnayakingsike@gmail.com>
2023-03-23 23:29:52 +0100

    sanity

---
+++ src/client.js
@@ -0,0 +1,12 @@
+
+
+import { createClient } from '@sanity/client'
+
+
+export const client = createClient({
+    projectId: 'hdfw4xwd',
+    dataset: 'production',
+    useCdn: false, // set to `true` to fetch from edge cache
+    apiVersion: '2022-01-12', // use current date (YYYY-MM-DD) to target the latest API version
+    // token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
+})
